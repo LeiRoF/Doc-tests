4322efbc8b97d7d61aece8824f9bb63c626784ad
Vincent (Leirof) <vince.lrf@gmail.com>
2021-11-16 23:06:18 +0100

    Test 3

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,10 @@ extensions = [
     'sphinx.ext.mathjax'
 ]
 
+mathjax3_config = {
+    'tex': {'tags': 'ams', 'useLabelIds': True},
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
